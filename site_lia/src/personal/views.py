abd718997f2f5932ff70dfd0150ad83cfec15216
henrique <henrique.tavares@fatec.sp.gov.br>
2023-10-27 08:39:55 -0300

    continuacao das aulas, até aula 11

--- site_lia/src/personal/views.py
+++ site_lia/src/personal/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 
 def home_screen_view(request):
-
     context = {}
+    questions = Question.objects.all()
+    context['questions'] = questions
+
+    '''
+    
     context['some_string'] = 'this is some string que estou passando para a view'
     context['some_number'] = 34252708
+    context['nome'] = "Henrique"
 
 
     context = {
         'some_string': 'segunda forma de contexto',
         'some_number': 892720,
+        'nome': "Henrique",
     }
 
     list_of_values = []
@@ -20,6 +27,5 @@ def home_screen_view(request):
     list_of_values.append("terceiro")
     list_of_values.append("quarto")
     context['list_of_values'] = list_of_values
-
-
+    '''
     return render(request, "personal/home.html", context)
